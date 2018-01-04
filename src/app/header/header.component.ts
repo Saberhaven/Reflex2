c7c602d1966a4f505703197fd739143e91694db7
Black <L7D23B@valero.com>
2018-01-04 14:04:13 -0600

    'Added'

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SsoService } from '../sso.service'
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [SsoService]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  private user;
+  constructor(private ssoService: SsoService) { }
 
   ngOnInit() {
+    this.getSso();
+
+  }
+
+  getSso() {
+    this.ssoService.getSso().subscribe(
+      data => {this.user = data},
+      err => console.error(err),
+      () => console.log(this.user)
+    );
     
   }
 
---
+++ src/app/sso.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class SsoService {
+  apiURL = "./assets/sso.api.php"
+  constructor(private http: HttpClient) { }
+
+  getSso() {
+    return this.http.get(this.apiURL, {
+    });
+  }
+
+}
